3acac931cac151200bc4601534d61624e9e745ba
abetor <Molaytuh@gmail.com>
2019-01-28 22:12:56 +0500

    Lesson 18-26
    Two Activity

---
+++ p0211twoactivity/src/main/java/com/example/android/p0211twoactivity/MainActivity.java
@@ -0,0 +1,32 @@
+package com.example.android.p0211twoactivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.view.View;
+import android.widget.Button;
+
+public class MainActivity extends AppCompatActivity {
+
+    Button btnActTwo;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        btnActTwo = findViewById(R.id.btnActTwo);
+        btnActTwo.setOnClickListener((View.OnClickListener) this);
+    }
+
+    public void onClick(View v) {
+        switch (v.getId()) {
+            case R.id.btnActTwo:
+                Intent intent = new Intent(this, ActivityTwo.class);
+                startActivity(intent);
+                break;
+            default:
+                break;
+        }
+    }
+}
