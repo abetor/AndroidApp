192cae87940eb32030265990580dc63be11a9fc3
abetor <Molaytuh@gmail.com>
2019-02-07 21:25:29 +0500

    Lesson 61-80
    Files, parcel, preference, files, tabs, handler

---
+++ p0691parcelable/src/main/java/com/example/android/p0691parcelable/SecondActivity.java
@@ -0,0 +1,20 @@
+package com.example.android.p0691parcelable;
+
+import android.app.Activity;
+import android.os.Bundle;
+import android.util.Log;
+
+public class SecondActivity extends Activity {
+
+    final String LOG_TAG = "myLogs";
+
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.second);
+        Log.d(LOG_TAG, "getParcelableExtra");
+        MyObject myObj = (MyObject) getIntent().getParcelableExtra(
+                MyObject.class.getCanonicalName());
+        Log.d(LOG_TAG, "myObj: " + myObj.s + ", " + myObj.i);
+    }
+
+}
